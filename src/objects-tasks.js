a58e95b9b3c71ec22b62b8cf2044ed86c084aad7
Helena <gomel-offis@mail.ru>
2025-03-06 00:24:59 +0300

    feat: add task 2

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const mergedObject = objects.reduce((acc, obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      acc[key] = (acc[key] || 0) + value;
+    });
+    return acc;
+  }, {});
+  return mergedObject;
 }
 
 /**
